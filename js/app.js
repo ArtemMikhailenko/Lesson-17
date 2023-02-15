9e3214e15dbbb3a263b781835ac75261e6becfd3
ArtemMikhailenko <Artemlend@gmail.com>
2023-02-15 21:32:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 console.log('You can ignore single and double quotes like this:\\\'\\\' \\" \\"')
 
+console.log(Boolean(alert('5'))) //false
+console.log(true>false)  //true тому що true це 1 , а false це 0 , а 1>0
+console.log(true>5)  //false 1<5
+console.log(true<'5') //true 5 в string зводиться до числа і 1<5
+console.log(true>null) //true null=0 , 1>0
+
 alert('Please enter your data')
 let userName = prompt("Enter your name", 'Name')
 alert(`Your first name:${userName}`)
